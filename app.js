1ea31bd07dd12915aa6cc8c195c831dcea12f4ff
IReaxy <60553691+IReaxy@users.noreply.github.com>
2023-09-09 21:05:48 +0300

    halfU

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const API_Key = "" //put the key here
+const submitIcon = document.querySelector("#submit-icon");
 
+const getImages = () => {
+
+        try {
+
+            fetch( //pull URL here , options)
+            }
+            catch (error) {
+                console.error(error)
+
+            }
+
+
+        }
+
+
+        submitIcon.addEventListener('click', getImages)
