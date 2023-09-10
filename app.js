b977354effc7b549c58fb90b952ab60f91054aed
IReaxy <60553691+IReaxy@users.noreply.github.com>
2023-09-10 13:40:36 +0300

    end

--- app.js
+++ app.js
@@ -7,32 +7,33 @@ const imageSection = document.querySelector(".image-section")
 const getImages = async() => {
 
     const options = {
-        method: "POST"
-        headers: {
+        method: "POST",
+        Headers: {
             "Authorization": Bearer $ { API_KEY },
-            "content-Type": "application/json"
+            "Content-Type"
+            "application/json"
 
         },
-        body: JSON.stringfy({
+        body: JSON.stringify({
 
-            "prompt": inputElement.value,
-            "n": 4,
-            "size": "256x256 "
+            prompt: inputElement.value,
+            n: 4,
+            size: "256 x256 "
         })
     }
 }
 
 try {
 
-    const response = await fetch('', options)
+    const response = await fetch("", options)
     const data = await response.json()
     data ? .data.array.forEach(imageObject => {
-        constImageContainer = document.createElement('div')
-        ImageContainer.classList.add('image-container')
+        const imageContainer = document.createElement("div")
+        imageContainer.classList.add("image-container")
         const imageElement = document.createElement("img")
         imageElement.setAttribute("src", imagejet.url)
-        ImageContainer.append(imageElement)
-        imageSection.append(ImageContainer)
+        imageContainer.append(imageElement)
+        imageSection.append(imageContainer)
 
     });
 } catch (error) {
